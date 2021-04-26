1c48a8273aa661a30e28230008fd6b27a5e6b38b
Devin Dyson <dyson@bu.edu>
2021-04-26 14:43:07 -0400

    p5

---
+++ problem5.py
@@ -0,0 +1,22 @@
+sumsq = 0
+q = 0
+sqsum = 0
+solution = 0
+
+for i in range(100):
+	n = (i + 1)*(i + 1)
+	sumsq  += n
+
+print(sumsq) 
+
+for x in range(100):
+	x = x + 1
+	q  += x
+
+sqsum = q*q
+
+print(sqsum)
+
+solution = sqsum - sumsq
+
+print(solution)
