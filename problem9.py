8ff8201caa8913d1d5ad6cb41fa4a1edf2f3c67f
Devin Dyson <dyson@bu.edu>
2021-04-26 19:17:32 -0400

    problem 9

---
+++ problem9.py
@@ -0,0 +1,10 @@
+def pythagorean_triplet():
+	for a in range(1,1000):
+		for b in range(1,1000):
+			c = 1000-a-b
+			if (a**2+b**2) == c**2:
+				return a*b*c
+
+solution = pythagorean_triplet()
+
+print(solution)
